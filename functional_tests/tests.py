fff0f50d459150e0303ead552243fa358d48034e
imed.mnif <imed.mnif@gmail.com>
2016-06-19 14:30:10 +0100

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -67,7 +67,7 @@ class NewVisiorTest(LiveServerTestCase):
         self.browser = webdriver.Chrome(self.BROWSER_PATH)
 
         # Francis visits the home page. There's no sign of Edith's list
-        self.browser.get(self.liver_server_url)
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -81,7 +81,7 @@ class NewVisiorTest(LiveServerTestCase):
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
-        self.assertNotEqual(francis_list_url, edith|_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
