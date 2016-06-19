b054591ad6dc687cebec1d18228873d44ed0f3d3
imed.mnif <imed.mnif@gmail.com>
2016-06-19 01:49:16 +0100

    Refactor functional tests to use helper check method

--- functional_tests.py
+++ functional_tests.py
@@ -8,8 +8,16 @@ class NewVisiorTest(unittest.TestCase):
         self.browser = webdriver.Chrome('/cygdrive/c/work/tools/chromedriver_win32/chromedriver.exe')
         self.browser.implicitly_wait(3)
 
+
     def tearDown(self):
         self.browser.quit()
+
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -37,9 +45,7 @@ class NewVisiorTest(unittest.TestCase):
         #import time
         #time.sleep(10)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -48,10 +54,8 @@ class NewVisiorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -61,6 +65,7 @@ class NewVisiorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied she goes back to sleep
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
