0f1788db0dcc89ce2e41fc2f0fb733e4cec24d54
imed.mnif <imed.mnif@gmail.com>
2016-07-03 04:27:02 +0100

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -91,8 +91,26 @@ class NewVisiorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
         # Satisfied they both go back to sleep
+
+
+    def test_layout_and_styling(self):
+        # Edith goes to home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=5)
+
+        # She starts a new list and sees the input is nicely centered to
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=5)
+
