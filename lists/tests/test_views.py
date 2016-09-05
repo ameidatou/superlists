a893154a08aae480a633009520eecbe7dfa5f03b
imed.mnif <imed.mnif@gmail.com>
2016-09-05 23:16:16 +0100

    Refacator list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -59,6 +59,28 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post('/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post('/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'})
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
 class NewListTest(TestCase):
 
 
@@ -92,7 +114,7 @@ class NewListTest(TestCase):
 
 
 
-class NewItemTest(TestCase):
+"""class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
@@ -107,7 +129,7 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
@@ -115,4 +137,5 @@ class NewItemTest(TestCase):
             data={'item_text': 'A new item for an existing list'})
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+"""
 
